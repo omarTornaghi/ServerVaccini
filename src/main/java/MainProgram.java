12ea7b679a5992e9997d47d26fb75cae50bcec3f
omarTornaghi <omar.tor2011@gmail.com>
2021-08-25 18:10:09 +0200

    Inizializzazione del db

--- src/main/java/MainProgram.java
+++ src/main/java/MainProgram.java
@@ -10,17 +10,23 @@ import java.sql.Date;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class MainProgram {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
         boolean inizializzazione = Initializer.initialize();
-        if(!inizializzazione){
+        while(!inizializzazione){
             System.err.println("Impossibile inizializzare le risorse");
-            return;
+            System.out.print("Riprovare?[S/N]");
+            String c = scanner.next();
+            if(c.toLowerCase(Locale.ROOT).equals("s"))
+                inizializzazione = Initializer.initialize();
+            else
+                System.exit(0);
         }
         ServerHandler server = new ServerHandler();
         server.execute();
     }
-
-
 }
